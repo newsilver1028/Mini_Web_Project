73dec4c4877dd24841eb6d0bd330ced034554cc0
GAEUN SHIN <newsilver1028@naver.com>
2021-06-19 15:35:21 +0900

    Update DAY3

--- Carousel/index.js
+++ Carousel/index.js
@@ -1,1 +1,37 @@
-console.log("hello, vanilla.");
+var index = 1;
+showSlides(index);
+
+function nextSlide(n) {
+    showSlides((index += n));
+}
+
+function prevSlide(n) {
+    showSlides((index -= n));
+}
+
+function currentSlide(n) {
+    showSlides((index = n));
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("images-wrapper");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        index = 1;
+    }
+    if (n < 1) {
+        index = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className.replace("active", "");
+    }
+
+    slides[index - 1].style.display = "block";
+    // dots[index - 1].className += "active";
+}
