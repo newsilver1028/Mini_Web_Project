826a54196776260429a9f876807831bf23e877a4
GAEUN SHIN <newsilver1028@naver.com>
2021-06-29 17:19:03 +0900

    Labeling week

--- Calendar/index.js
+++ Calendar/index.js
@@ -1,1 +1,16 @@
-console.log("hello, vanilla.");
+var date = new Date();
+var week = new Array("SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT");
+
+var day = document.getElementById("day");
+day.innerHTML = week[date.getDay()];
+
+var todate = document.getElementById("date");
+todate.innerHTML = date.getDate();
+
+var mon = new Array("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC");
+var month = document.getElementById("month");
+month.innerHTML = mon[date.getMonth()] + " " + date.getFullYear();
+
+var joinWeek = week.join(" ");
+var weekText = document.getElementById("week");
+weekText.innerHTML = joinWeek;
