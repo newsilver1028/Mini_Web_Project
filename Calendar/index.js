d8c9a7979eaa57534f3a2dcbb3739d98907e3c8d
GAEUN SHIN <newsilver1028@naver.com>
2021-06-30 16:58:25 +0900

    Create calendar table

--- Calendar/index.js
+++ Calendar/index.js
@@ -11,6 +11,16 @@ var mon = new Array("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP
 var month = document.getElementById("month");
 month.innerHTML = mon[date.getMonth()] + " " + date.getFullYear();
 
-var joinWeek = week.join(" ");
-var weekText = document.getElementById("week");
-weekText.innerHTML = joinWeek;
+function createCal() {
+    var firstDate = new Date(date.getFullYear, date.getMonth, 1);
+    var lastDate = new Date(date.getFullYear, today.getMonth() + 1, 0);
+
+    var tb = document.getElementById("calendar");
+    var tb2 = document.getElementById("tbday");
+
+    while (tb.rows.length > 2) {
+        tb.deleteRow(tb.rows.length - 1);
+    }
+    var row = null;
+    row = tb.insertRow();
+}
